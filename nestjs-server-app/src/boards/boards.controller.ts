41f00ccc64d9cdc5dc9a1d87ad14446633ca6a63
ParkDongJun <yuki@ubisam.com>
2024-06-14 15:40:08 +0900

    내용 업데이트

--- nestjs-server-app/src/boards/boards.controller.ts
+++ nestjs-server-app/src/boards/boards.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Board } from './board.model';
 import { BoardsService } from './boards.service';
+import { CreateBoardDto } from './dto/creact-board.dto';
 
 const log = (message: string) => {
     const timestamp = new Date().toISOString();
@@ -21,12 +22,36 @@ export class BoardsController {
     //     log(`Received body: ${JSON.stringify(body)}`);
     // }
 
+    // @Post()
+    // createBoard(
+    //     @Body('title') title: string,
+    //     @Body('description') description: string
+    // ): Board{
+    //     console.log('title',title);
+    //     console.log('title',description);
+    //     return this.boardsService.createBoard(title, description);
+    // }
+
+    // interface 사용시
+    // @Post()
+    // createBoard(
+    //     @Body('title') title: string,
+    //     @Body('description') description: string
+    // ): { message: string } {  // Return a success message as an object
+    //     log(`Received title: ${title}`);
+    //     log(`Received description: ${description}`);
+        
+    //     this.boardsService.createBoard(title, description);
+    //     return { message: '성공' };  // Custom success message
+    // }
+
+    // Dto 사용시
     @Post()
     createBoard(
-        @Body('title') title: string,
-        @Body('description') description: string
-    ): Board{
-        return this.boardsService.createBoard(title, description);
+        @Body() createBoardDto: CreateBoardDto
+    ): Board {  
+        return this.boardsService.createBoard(createBoardDto);
     }
+    
 }
 
--- nestjs-server-app/src/boards/boards.service.ts
+++ nestjs-server-app/src/boards/boards.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/creact-board.dto';
 
 @Injectable()
 export class BoardsService {
@@ -10,7 +11,22 @@ export class BoardsService {
         return this.boards;
     }
 
-    createBoard(title: string, description: string){
+    // createBoard(title: string, description: string){
+    //     const board: Board = {
+    //         id: uuid(),
+    //         title: title,
+    //         description: description,
+    //         status: BoardStatus.PUBLIC
+    //     }
+
+    //     this.boards.push(board);
+    //     return board;
+    // }
+
+    //DTO 사용시 생성
+    createBoard(createBoardDto: CreateBoardDto) {
+        const { title, description } = createBoardDto;
+
         const board: Board = {
             id: uuid(),
             title: title,
